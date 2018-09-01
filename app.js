dc1ef7e4ca79e39753986ee3eab061bd62482a13
Justinon <jglommen@gmail.com>
2018-09-01 12:14:41 -0700

    Wrapped statements in functions for clarity

--- app.js
+++ app.js
@@ -8,103 +8,106 @@ var http = require('http');
 var path = require('path');
 var handlebars = require('express3-handlebars');
 
-/* Web3 and Loom dealio  */
+// TODO: Figure out how to use web3js in 'public/js/custom.js' for async await calls
+var web3js;
+var privateKey;
+var publicKey;
 
-// const Web3 = require('web3');
+/* Web3 and Loom dealio  */
 import Web3 from 'web3';
 import SimpleStore from './public/SimpleStore.json';
 import { Client, LocalAddress, CryptoUtils, LoomProvider } from 'loom-js';
 
-const privateKey = CryptoUtils.generatePrivateKey();
-const publicKey = CryptoUtils.publicKeyFromPrivateKey(privateKey);
-
-// Create the client
-// TODO: Get rid of the secret here. There should be no commiting of secrets to code,
-// TODO: especially a public repo like this. Needs to be injected through a config file.
-const clientIp = "192.168.86.72"
-const client = new Client(
-  'default',
-  // have yet to actually get this working externally
-  `ws://${clientIp}:46657/websocket`,
-  `ws://${clientIp}:9999/queryws`,
-);
-
-// The address for the caller of the function
-const from = LocalAddress.fromPublicKey(publicKey).toString();
-
-// Instantiate Web3 client using LoomProvider
-const web3 = new Web3(new LoomProvider(client, privateKey));
-// TODO: Use a real logger lib instead of the console logger.
-// TODO: Needs to be able to specify log_level so we can control output
-console.log("CURRENT PROVIDER");
-console.log(web3.currentProvider);
-
-const ABI = SimpleStore.abi;
-console.log(ABI);
-
-const contractAddress = '0x1a31b9b9d281d49001fe7f3f638000a739afc9c3';
-
-// Instantiate the contact and let it be ready to use
-const contract = new web3.eth.Contract(ABI, contractAddress, {from});
-console.log("CONTRACT");
-console.log(contract);
-
-//const loomContractAddress = client.getContractAddressAsync("SimpleStore")
-//console.log(loomContractAddress);
-
-contract.events.NewValueSet({}, (err, event) => {
-  if(err) {
-    return console.error(err);
-  }
-  console.log('New value set', event.returnValues._value);
-});
-
-(async function () {
-  // Set value of 47
-  const info = await contract.methods.set(47).send()
-  console.log("INFO:")
-  console.log(info)
-
-  // Get the value
-  const result = await contract.methods.get().call()
-  // result should be 47
-  console.log("RESULT should be 47:")
-  console.log(result)
-})()
-
-
-/* End of Web3 and Loom stuff */
-
-var index = require('./routes/index');
-// Example route
-// var user = require('./routes/user');
-
-var app = express();
-
-// all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.engine('handlebars', handlebars());
-app.set('view engine', 'handlebars');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(express.cookieParser('IxD secret key'));
-app.use(express.session());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
-
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+function createLoomClient(clientHost) {
+  return new Client(
+    'default',
+    `ws://${clientHost}:46657/websocket`,
+    `ws://${clientHost}:9999/queryws`,
+  );
 }
 
-app.get('/', index.view);
-// Example route
-// app.get('/users', user.list);
+function setWeb3Provider(loomClient) {
+  privateKey = CryptoUtils.generatePrivateKey();
+  publicKey = CryptoUtils.publicKeyFromPrivateKey(privateKey);
+  web3js = new Web3(new LoomProvider(loomClient, privateKey));
+}
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Hi, Ryan! Listening on port ' + app.get('port'));
-});
+function setContract(address, ABI) {
+  // TODO: Use a real logger lib instead of the console logger.
+  //       Needs to be able to specify log_level so we can control output
+  console.log(ABI);
+  //const loomContractAddress = client.getContractAddressAsync("SimpleStore")
+  //console.log(loomContractAddress);
+  const from = LocalAddress.fromPublicKey(publicKey).toString(); // The address for the caller of the function
+  const contract = new web3.eth.Contract(ABI, address, {from});
+  console.log("CONTRACT");
+  console.log(contract);
+  return contract;
+}
+
+function registerContractEvents(contract) {
+  contract.events.NewValueSet({}, (err, event) => {
+    if(err) {
+      return console.error(err);
+    }
+    console.log('New value set', event.returnValues._value);
+  });
+}
+
+function initApp() {
+  window.addEventListener('load', function() {
+    // Checking if Web3 has been injected by the browser (Mist/MetaMask)
+    if (typeof web3 !== 'undefined') {
+      console.log('Cannot use current web3 provider. Initializing Loom Provider...');
+    }
+    // TODO: Get rid of the host here. There should be no commiting of secrets to code,
+    //       especially a public repo like this. Needs to be injected through a config file.
+    const clientHost = "192.168.86.72";
+    setWeb3Provider(createLoomClient(clientHost));
+    const contractAddress = '0x1a31b9b9d281d49001fe7f3f638000a739afc9c3';
+    registerContractEvents(setContract(contractAddress, SimpleStore.abi));
+    startApp();
+  })
+}
+
+function startApp() {
+  var index = require('./routes/index');
+  // Example route
+  // var user = require('./routes/user');
+
+  var app = express();
+
+  // all environments
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', path.join(__dirname, 'views'));
+  app.engine('handlebars', handlebars());
+  app.set('view engine', 'handlebars');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.json());
+  app.use(express.urlencoded());
+  app.use(express.methodOverride());
+  app.use(express.cookieParser('IxD secret key'));
+  app.use(express.session());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+
+  // development only
+  if ('development' == app.get('env')) {
+    app.use(express.errorHandler());
+  }
+
+  app.get('/', index.view);
+  // Example route
+  // app.get('/users', user.list);
+
+  startServer();
+}
+
+function startServer() {
+  http.createServer(app).listen(app.get('port'), function(){
+    console.log('Hi, User! Listening on port ' + app.get('port'));
+  });
+}
+
+initApp();
