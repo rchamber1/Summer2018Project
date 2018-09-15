657a84b64a72c5adf386f1e84125f8265c26330c
Justinon <jglommen@gmail.com>
2018-09-15 13:02:23 -0700

    commented out a block that wont work yet

--- public/js/contracts/persona_contract.js
+++ public/js/contracts/persona_contract.js
@@ -13,11 +13,11 @@ export default class PersonaContract extends Contract {
   
   // TODO: Make this function register events from THIS contract!
   registerContractEvents() {
-    this.contract.events.NewValueSet({}, (err, event) => {
-      if(err) {
-        return console.error(err);
-      }
-      console.log('New value set', event.returnValues._value);
-    });
+    // this.contract.events.NewValueSet({}, (err, event) => {
+    //   if(err) {
+    //     return console.error(err);
+    //   }
+    //   console.log('New value set', event.returnValues._value);
+    // });
   }
 }
